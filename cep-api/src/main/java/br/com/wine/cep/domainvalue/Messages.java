2269d0e83f51f7bd9d725b8b6e330e1e57a09cdd
guede <guede@192.168.0.135>
2021-06-03 15:11:54 -0400

    Commit!

---
+++ cep-api/src/main/java/br/com/wine/cep/domainvalue/Messages.java
@@ -0,0 +1,6 @@
+package br.com.wine.cep.domainvalue;
+
+public class Messages {
+
+	public static final String CEP_DTO_STRING = "{\"id\":null,\"codigoLoja\":\"LOJA_ITAIM\",\"faixaInicio\":50000001,\"faixaFim\":60000000}";
+}
--- cep-api/src/test/java/br/com/wine/cep/controller/CepControllerTest.java
+++ cep-api/src/test/java/br/com/wine/cep/controller/CepControllerTest.java
@@ -1,5 +1,65 @@
 package br.com.wine.cep.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import br.com.wine.cep.domainvalue.Messages;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
 public class CepControllerTest {
 
+	@Autowired
+	private MockMvc mvc;
+	
+	@Test
+	public void shouldReturnOKStatusOnGetAll() throws Exception {
+		
+		this.mvc.perform(get("/cep")
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
+	}
+	
+	@Test
+	public void shouldReturnACreatedStatus() throws Exception {
+		
+		this.mvc.perform(post("/cep")
+				.accept(MediaType.APPLICATION_JSON)
+				.contentType(MediaType.APPLICATION_JSON_VALUE)
+				.content(Messages.CEP_DTO_STRING))
+				.andExpect(status().isCreated());
+	}
+	
+	@Test
+	public void shouldReturnOkStatusOnGetCep() throws Exception {
+		
+		this.mvc.perform(get("/cep/15486125")
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
+	}
+	
+	@Test
+	public void shouldReturnOkOnDeleteCep() throws Exception {
+		
+		this.mvc.perform(delete("/cep/1")
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
+	}
+	
+	@Test
+	public void shouldReturnErrorOnDeleteCep() throws Exception {
+		
+		this.mvc.perform(delete("/cep/100")
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().is4xxClientError());
+	}
 }
