a82396b2deddeb1023f3a19ece4d8eb6bf8cf476
guede <guede@192.168.0.135>
2021-06-03 13:26:25 -0400

    Commit!

---
+++ cep-api/src/main/resources/import.sql
@@ -0,0 +1,8 @@
+
+insert into cep (id, codigo_loja, faixa_inicio, faixa_fim) values (1, 'LOJA PINHEIROS', 10000000, 20000000);
+
+insert into cep (id, codigo_loja, faixa_inicio, faixa_fim) values (2, 'LOJA PINHEIROS', 20000001, 30000000);
+
+insert into cep (id, codigo_loja, faixa_inicio, faixa_fim) values (3, 'LOJA JARDINS', 30000001, 40000000);
+
+insert into cep (id, codigo_loja, faixa_inicio, faixa_fim) values (4, 'LOJA JARDINS', 40000001, 50000000);
--- cep-api/src/test/java/br/com/wine/cep/controller/CepControllerTest.java
+++ cep-api/src/test/java/br/com/wine/cep/controller/CepControllerTest.java
@@ -1,17 +1,5 @@
 package br.com.wine.cep.controller;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-
-@WebMvcTest
 public class CepControllerTest {
 
-	@Autowired
-	private CepController cepController;
-	
-	@Test
-	public void teste() {
-		
-	}
 }
